1377713cd9adc9c996fc0ea2192c26c873053078
Azure DevOps <noreply@flipdish.com>
2026-09-22 13:42:15 +0000

    Auto generated by swagger-codegen-cli

--- src/Flipdish/Model/SmsProviderCredential.cs
+++ src/Flipdish/Model/SmsProviderCredential.cs
@@ -58,13 +58,7 @@ namespace Flipdish.Model
             /// Enum MessageBird for value: MessageBird
             /// </summary>
             [EnumMember(Value = "MessageBird")]
-            MessageBird = 4,
-            
-            /// <summary>
-            /// Enum CommunicationService for value: CommunicationService
-            /// </summary>
-            [EnumMember(Value = "CommunicationService")]
-            CommunicationService = 5
+            MessageBird = 4
         }
 
         /// <summary>
